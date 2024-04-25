8d08b276bea3de66db6c5376bdea24fdf6965c7c
dgutper960 <dgutper960@outlook.es>
2024-04-25 14:01:04 +0200

    limpia contenido basico

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -10,7 +10,4 @@
  * Write any other JavaScript below
  */
 
-+( function() {
-  const university = "UOC";
-  console.log(`Hello, ${university}!`);
-} )();
+console.log("Welcome to: Web Ana de Consuelo");
